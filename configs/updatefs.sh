d49180328c3c9c15a78a498b16e87346c614e3d9
satishagrwal <satish.agrwal@gmail.com>
2021-01-22 15:59:10 +0530

    Update updatefs.sh

--- configs/updatefs.sh
+++ configs/updatefs.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+#configure default password
+sed -i -e 's/.*<X-PRE-PROCESS cmd="set" data="default_password=1234"\/>.*/<X-PRE-PROCESS cmd="set" data="default_password=freeswitchplivo7809"\/>/g' /etc/freeswitch/vars.xml
+
+#configure to restrict blind register
+sed -i -e 's/.*<!-- <param name="accept-blind-reg" value="true"\/> -->.*/<param name="accept-blind-reg" value="false"\/>/g' /etc/freeswitch/sip_profiles/internal.xml
